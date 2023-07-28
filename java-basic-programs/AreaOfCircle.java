05140fccc47ec9ab1788d0b2cb7e07011d22b3bf
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2023-07-28 23:06:49 +0530

    Update AreaOfCircle.java

--- java-basic-programs/AreaOfCircle.java
+++ java-basic-programs/AreaOfCircle.java
@@ -1,1 +1,25 @@
+package com.basicprograms;
+
+import java.util.Scanner;
+
+public class AreaOfCircle {
+
+	public static void main(String[] args) {
+		 int rad =0;
+		 double pi= 3.14;
+		 double ans=0;
+		
+		System.out.println("Enter the radius of the circle :");
+		
+		Scanner sc = new Scanner(System.in);
+		rad = sc.nextInt();
+		System.out.println(" JAVA PI Value"+Math.PI);
+		
+		
+		ans = pi * rad * rad;
+		System.out.println("Area of the circle is:"+ans);
+		
+	}
+	
+}
 
